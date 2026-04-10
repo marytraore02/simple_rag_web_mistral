e11ae353d208864332395386403c4009f1dd2752
marytraore02 <marytra22@gmail.com>
2026-04-10 18:24:00 +0000

    feat(utils): export query classifier in utils package
    Expose classify_query_intent, INTENT_RAG and INTENT_CHAT constants
    from the new query_classifier module for clean imports.

--- streamlit/utils/__init__.py
+++ streamlit/utils/__init__.py
@@ -1,3 +1,4 @@
 """Fonctions utilitaires pour l'interface Streamlit."""
 
 from utils.helpers import human_size, file_icon, index_ready  # noqa: F401
+from utils.query_classifier import classify_query_intent, INTENT_RAG, INTENT_CHAT  # noqa: F401
